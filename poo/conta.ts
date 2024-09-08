a62b406ca936bf4b1125d72024f0b12276b7836b
Dev Vitor <160429477+DevVitorlevi@users.noreply.github.com>
2024-09-08 10:22:44 -0300

    att

--- poo/conta.ts
+++ poo/conta.ts
@@ -8,7 +8,7 @@ class conta_padrao extends conta{
 }
 class conta_premium extends conta{
     static tarifa(valor: number):number{
-        const desconto = valor * 0.2
+        const desconto:number = valor * 0.2
         return valor - desconto
     }
 }
