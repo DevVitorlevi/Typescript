39a57e8f26c54b3bd6721a0d45b35dcdd7c37408
Vitor Levi <160429477+DevVitorlevi@users.noreply.github.com>
2025-04-04 07:37:13 -0300

    att

---
+++ A001-type-an/type-any.ts
@@ -0,0 +1,9 @@
+function showMessage(msg: any) {
+    return msg
+}
+
+console.log(showMessage('Olá Mundo'))
+console.log(showMessage([1, 2, 3]))
+console.log(showMessage(1.2))
+
+//Any Geralmente não é para ser usado com frequência, ultiliza-se ele em ultimo caso
--- A001-type-an/type.ts
+++ A001-type-an/type.ts
@@ -1,23 +1,30 @@
 const nome: string = "Joaquim";
 const idade: number = 32;
 const adulto: boolean = true;
-
+console.log(nome, idade, adulto)
 //Objetos 
 
 let pessoa: { nome: string, idade: number, adulto?: boolean } = {
     nome: "Joaquim",
     idade: 32
 }
+console.log(pessoa.nome)
+console.log(pessoa.idade)
 
 //Funções
 
 function soma(a: number, b: number): number {
     return a + b
 }
-
+console.log(soma(2, 3))
 //Arrays
 
 let ArrayNumber: number[] = [1, 2, 3, 4, 5]
 let ArrayString: string[] = ["a", "b", "c"]
 let ArrayNumber2: Array<number> = [1, 2, 3, 4, 5]
 let ArrayString2: Array<string> = ["a", "b", "c"]
+
+console.log(ArrayNumber)
+console.log(ArrayNumber2)
+console.log(ArrayString)
+console.log(ArrayString2)
