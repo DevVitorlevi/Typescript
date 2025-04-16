8dfbc0c5343d1767b0a76e883a888624d8ac6047
Vitor Levi <160429477+DevVitorlevi@users.noreply.github.com>
2025-04-16 10:57:11 -0300

    att

---
+++ Desafios/Sorteio.ts
@@ -0,0 +1,10 @@
+function sortNumber(num: number) {
+    let numcompare = Math.floor(Math.random() * 10) + 1
+    if (num === numcompare) {
+        return console.log(`Parabens! O Numero Sorteado foi: ${numcompare}`)
+    }
+
+    return console.log(`Que Pena O Numero Sorteado Foi:$ {numcompare}`)
+}
+
+sortNumber(5)
